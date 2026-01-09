121fef12bf7cb560fc6123d503ffcf1a395b6d0d
abdullah-mobin <mahi.odyssey@gmail.com>
2026-01-09 22:22:06 +0600

    feat: make tts api free

--- routes/generate.go
+++ routes/generate.go
@@ -2,13 +2,12 @@ package routes
 
 import (
 	"github.com/abdullah-mobin/helloBanglaTTS-backend/handlers"
-	"github.com/abdullah-mobin/helloBanglaTTS-backend/middlewares"
 	"github.com/gofiber/fiber/v2"
 )
 
 func GenerateRoutes(route fiber.Router) {
 
-	route.Post("/tts", middlewares.IsAuthenticated, handlers.GenerateTTS)
+	route.Post("/tts", handlers.GenerateTTS)
 	// route.Post("/video")
 	// route.Post("/image")
 	// route.Post("/story")
